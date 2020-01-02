dfdfa763b66a845c1fd4ee33b4a7134309fcaf86
Sebastian <sebasgrammar@hotmail.com>
2020-01-02 17:57:40 -0500

    Added dinamic menus

--- main.js
+++ main.js
@@ -9,6 +9,44 @@ function clearAll() {
     display.innerHTML = ""
 }
 
+function createMenu() {
+
+    let menu = document.createElement("div")
+    menu.classList.add("nutritional-info")
+    menu.innerHTML =
+    `
+    <div class="headings row">
+        <label></label>
+        <label>Quantity</label>
+        <label>Portion amount</label>    
+        <label>Calories</label>
+    </div>
+    <div class="row">
+        <div class="buttons">
+            <button>-</button>
+            <button>+</button>
+        </div>
+        <div class="quantity"></div>
+        <div class="portion-amount"></div>
+        <div class="calories"></div>
+    </div>
+    `
+    return menu
+
+}
+
+function deployMenu() {
+
+    //this.style.display = "block"
+
+    if (this.style.display == "none") {
+        this.style.display = "block"
+    } else {
+        this.style.display = "none"
+    }
+
+}
+
 clear.addEventListener("click", clearAll)
 
 async function getData(input) {
@@ -27,12 +65,45 @@ async function getData(input) {
 
             if (name.toLowerCase().includes(input)) {
 
-                let n = document.createElement("div")
+
+
+
+                let foodName = document.createElement("div")
                 console.log(name)
-                n.classList.add("result")
-                n.textContent = name
-                n.style.background = "red"
-                display.appendChild(n)
+                foodName.classList.add("result")
+                foodName.textContent = name
+                foodName.style.background = "red"
+                display.appendChild(foodName)
+
+
+
+                let menu = createMenu()
+                foodName.addEventListener("click", deployMenu.bind(menu))
+
+
+                let necessaryElements = ["buttons", "quantity", "portion-amount", "calories"]
+                let text = ["", row[2], `${row[3]} ${row[4]}`, row[24]]
+
+
+                necessaryElements.forEach((thing, index) => {
+                    if (index > 0) {
+                        let currentItem = menu.querySelector(`.${thing}`)
+                        currentItem.textContent = text[index]
+                        console.log(`this is the current item: ${currentItem.textContent}`)
+                    }
+                })
+
+
+                /*
+                for (let counter = 0; counter < necessaryElements.length; counter++) {
+                    let currentItem = menu.querySelector(necessaryElements[counter])
+                    console.log(`this is the current item: ${currentItem}`)
+                }
+                */
+
+
+                display.appendChild(menu)
+                //display.appendChild( createMenu() )
             }
 
         }
